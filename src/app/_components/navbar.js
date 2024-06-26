452296a1d0d589309d1e56ba8d0b3072f9b0a617
DarrenNills <kikondanze@gmail.com>
2024-06-26 23:51:06 +0100

    Ajout de la navbar responsive

--- src/app/_components/navbar.js
+++ src/app/_components/navbar.js
@@ -1,24 +1,109 @@
 import "../globals.css";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/assets/logo phyto.png";
 
 export default function Navbar() {
-  return (
-    <>
-      <Link className="px-[5px]" href="/">
-        Accueil
-      </Link>
-      <Link className="px-[5px]" href="/produits">
-        Produits
-      </Link>
-      <Link className="px-[5px]" href="/temoignages">
-        Témoignages
-      </Link>
-      <Link className="px-[5px]" href="/actualites">
-        Actualités
-      </Link>
-      <Link className="px-[5px]" href="/a-propos">
-        A propos
-      </Link>
-    </>
-  );
+    return (
+        // <nav classNameName="w-full bg-slate-400">
+        //   <Link classNameName="px-[5px]" href="/">
+        //     Accueil
+        //   </Link>
+        //   <Link classNameName="px-[5px]" href="/produits">
+        //     Produits
+        //   </Link>
+        //   <Link classNameName="px-[5px]" href="/temoignages">
+        //     Témoignages
+        //   </Link>
+        //   <Link classNameName="px-[5px]" href="/actualites">
+        //     Actualités
+        //   </Link>
+        //   <Link classNameName="px-[5px]" href="/a-propos">
+        //     A propos
+        //   </Link>
+        // </nav>
+
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a
+                    href="/"
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
+                >
+                    <Image src={logo} alt="logo phytoscience" className="object-cover h-[50px] w-[160px]" />
+                </a>
+                <button
+                    data-collapse-toggle="navbar-default"
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-default"
+                    aria-expanded="false"
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 17 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
+                    </svg>
+                </button>
+                <div
+                    className="hidden w-full md:block md:w-auto"
+                    id="navbar-default"
+                >
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a
+                                href="/"
+                                className="block py-2 px-3  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                                aria-current="page"
+                            >
+                                Accueil
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/produits"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                            >
+                                Produits
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/temoignages"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                            >
+                                Témoignages
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/actualites"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                            >
+                                Actualités
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/a-propos"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                            >
+                                A propos
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
