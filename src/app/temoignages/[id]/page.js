ed7ee162123b4635e8bd64f331ea47e13bc279ab
DarrenNills <kikondanze@gmail.com>
2024-07-01 22:44:48 +0100

    Fix bug

--- src/app/temoignages/[id]/page.js
+++ src/app/temoignages/[id]/page.js
@@ -1,12 +1,11 @@
 import data from "../../data/temoignages";
 
 export default function Temoignages({ params }) {
-    const temoignage = data.filter((temoignage) => temoignage.id === parseInt(params.id));
-    console.log(temoignage)
+    const temoignage = data.find((temoignage) => temoignage.id === parseInt(params.id));
     return (
         <div className="">
-            <h1 className="text-black">{temoignage[0].titre}</h1>
-            <div dangerouslySetInnerHTML={{ __html: temoignage[0].description }} />
+            <h1 className="text-black">{temoignage.titre}</h1>
+            <div dangerouslySetInnerHTML={{ __html: temoignage.description }} />
         </div>
     );
 }
