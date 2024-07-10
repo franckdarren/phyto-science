34e8290e9715b13e82f3ff436f58f4d4d660656b
mando <mandochrist13@gmail.com>
2024-07-10 20:18:23 +0100

    bienvenu2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -95,7 +95,7 @@ const HeaderCarousel = () => {
                             className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 lg:h-full lg:w-auto"
                         />
                     </div>
-                    {/* <!-- Item 3 --> */}
+                    {/* <!-- Item trois --> */}
                     <div
                         className="hidden duration-700 ease-in-out"
                         data-carousel-item
