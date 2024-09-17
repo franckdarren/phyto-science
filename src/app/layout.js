75a07ffed9245469b3e414cba60f554baed3982c
Franck Darren <kikondanze@gmail.com>
2024-09-17 21:02:07 +0100

    modifs

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import Footer from "./_components/footer";
 import PreFooter from "./_components/pre-footer";
 import HotjarScript from "./_components/HotjarScript";
 export const metadata = {
-    title: "PhytoScience",
+    title: "PhytoScience Gabon",
     description:
         "Pionnier dans le traitement des pathologies lourdes avec cellules souches végétales, PhytoScience Gabon s'engage à offrir des solutions de pointe pour votre bien-être.",
 };
@@ -21,9 +21,10 @@ export default function RootLayout({ children }) {
                     href="/icon"
                     type="image"
                 />
+                <title>PhytoScience</title>
             </head>
             <body>
-            <HotjarScript />
+                <HotjarScript />
                 <Navbar />
                 <div>{children}</div>
                 <PreFooter />
