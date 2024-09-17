11c42d343d31559b9b00ac9653a33a3faa4e67d4
mando_christ <mandochrist13@gmail.com>
2024-09-17 20:37:38 +0100

    ajout du hotjar encore

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -763,7 +763,7 @@ const Contacts = () => {
                     d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863t-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3"
                 />
             </svg>
-            <p className="hover:text-blue-700 text-center font-regular text-[17px] pt-[5px]">
+            <p className="text-blue-700 text-center font-regular text-[17px] pt-[5px]">
                 <a href="tel:+241062104650">+ 241 062 10 46 50</a>
             </p>
             <svg
@@ -778,7 +778,7 @@ const Contacts = () => {
                     <path d="m4 9l7.106 3.553a2 2 0 0 0 1.788 0L20 9" />
                 </g>
             </svg>
-            <p className="hover:text-blue-600 text-center font-regular text-[17px] pt-[5px] pb-[50px]">
+            <p className="text-blue-600 text-center font-regular text-[17px] pt-[5px] pb-[50px]">
                 <a href="mailto:phytoscience@gmail.com">phytoscience@gmail.com</a>
             </p>
             <div className="pb-[50px]">
